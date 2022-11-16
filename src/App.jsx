2a0e59c878ee20f460b3891af16adf962ae613f9
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-11-16 20:38:16 +0300

    Add users APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Home from './page/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginScreen from './routes/Login';
+import SignupScreen from './routes/Signup';
+import { Route, Routes } from 'react-router-dom';
 import SideBarItem from './components/global/SideBar';
 
 function App() {
   return (
-    <>
-     <BrowserRouter>
-     <SideBarItem/>
-  <Routes>
- {/* <Route path="/" element={<Home />} /> */}
-  </Routes>
-    </BrowserRouter>
-    </>
-
-
-  )
-}
+    <div className="App">
+      <SideBarItem/>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LoginScreen />} path="/login" />
+          <Route element={<SignupScreen />} path="/signup" />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,37 +1,73 @@
-/* eslint linebreak-style: ["error", "unix"] */
+import React, { useState } from 'react';
 
-function Signup() {
+const SignupScreen = () => {
+  const [resp, setResp] = useState([]);
+
+  const register = async (e) => {
+    e.preventDefault();
+    const postBody = {
+      name: e.target.name.value,
+      username: e.target.username.value,
+      email: e.target.email.value,
+    };
+    const registration = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postBody),
+    };
+    const response = await fetch(`http://127.0.0.1:3000/users`, registration);
+    const data = await response.json()
+    const resp = [...resp];
+    resp = data
+    setResp(resp)
+  };
+
   return (
-    <div className="container page-login">
-      <form action="#" className="login-form" method="POST" id="signup-form">
-        <h2>SIGNUP</h2>
+    <>
+      <div className="container page-login">
+        <form onSubmit={register} className="login-form" method="POST" id="signup-form">
+          <h2>SIGNUP</h2>
 
-        <div className="add-padding-below">
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-field"
-            placeholder="Name"
-            required
-          />
-        </div>
-        <div className="add-padding-below">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="form-field"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <p className="signin-message">Already have an account?</p>
-        <div className="form-bottom-bar">
-          <button type="button">Send</button>
-        </div>
-      </form>
-    </div>
+          <div className="add-padding-below">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-field"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="add-padding-below">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="form-field"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className="add-padding-below">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-field"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <p className="signin-message">Already have an account?</p>
+          <div className="form-bottom-bar">
+            <button type="button">Send</button>
+          </div>
+        </form>
+      </div>
+    </>
   );
-}
-export default Signup;
+};
+
+export default SignupScreen;
