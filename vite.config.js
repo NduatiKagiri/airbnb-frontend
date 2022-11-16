5b90f43264dca6d11c9fa7bb0c064e10fc33499a
hedevedup <uzomaesse@gmail.com>
2022-11-16 22:21:47 +0100

    feat: removed vite

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
