69327f9490de5705610eef7cf3900aa46e8b4a78
Brenda Wihogora <brewihogora23@gmail.com>
2022-11-23 12:03:56 +0200

    Update signup.

--- src/pages/Home/Splash.jsx
+++ src/pages/Home/Splash.jsx
@@ -8,11 +8,11 @@ function Splash() {
   return (
     <div className="splash">
       <div className="overlay">
-        <Logo extraClasses="flex flex-col items-center aspect-square rounded-full bg-gradient-to-br from-yellow-400 to-yellow-500 text-white shadow-md text-4xl p-9" />
+        <Logo extraClasses="flex flex-col items-center aspect-square rounded-full bg-gradient-to-br from-yellow-100 to-yellow-500 text-white shadow-md text-4xl p-7" />
         <LogIn />
         <div className="add-form">
-          Or create a new account
-          <Link to="/sign_up" element={<SignUp />} className="submit-button bg-yellow-400">Sign Up</Link>
+          create a new account
+          <Link to="/sign_up" element={<SignUp />} className="submit-button bg-yellow-100">Sign Up</Link>
         </div>
       </div>
     </div>
