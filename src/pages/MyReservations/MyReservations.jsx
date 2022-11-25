389da8889a5df675c92102588661b34ed3c5318b
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-11-25 19:05:23 +0300

    Update MyReservations.jsx

--- src/pages/MyReservations/MyReservations.jsx
+++ src/pages/MyReservations/MyReservations.jsx
@@ -7,7 +7,7 @@ import getReservations from '../../redux/actions/Reservation/getReservation';
 
 function MyReservations() {
   const dispatch = useDispatch();
-  const { reservation } = useSelector((state) => state.reservations);
+  const { reservation, status } = useSelector((state) => state.reservations);
   const { houses } = useSelector((state) => state.houses);
 
   useEffect(() => {
@@ -20,9 +20,18 @@ function MyReservations() {
     return filterdHouse;
   };
 
-  if (!reservation) {
-    return <Loading message="Loading Reservations" />;
-  }
+  const reservedHouses = []
+  reservation.map((res, i) => {
+    houses.forEach((house) => {
+      if (res.house_id === house.id) {
+        const my_res = []
+        my_res.push(res)
+        my_res.push(house)
+        reservedHouses[i] = my_res
+      }
+    });
+  });
+
   return (
     <div className="pb-4">
       <div className="flex flex-col items-center justify-center py-16 md:pb-32">
@@ -30,7 +39,7 @@ function MyReservations() {
         <p className=" text-xs text-slate-400">Lists of all reservations.</p>
       </div>
       <div className="grid grid-cols-responsive px gap-4 justify-items-center">
-        {reservation.length > 0 && houses.length > 0 ? (reservation.map((res) => {
+        {status === 'success' && reservation.length > 0 && houses.length > 0 ? (reservation.map((res) => {
           const reservedHouse = house(res.house_id)[0];
           return (
             <HouseCard
